eda81a1d8b1e7f83310a2cb7fc20af82b0bcf50a
boworkgo <bowork189@gmail.com>
2019-07-30 18:04:41 -0400

    Test graph shows

--- posts/views.py
+++ posts/views.py
@@ -215,3 +215,22 @@ class DeleteView(generic.DeleteView):
 
     def get_success_url(self):
         return reverse("posts:index")
+
+from chartjs.views.lines import BaseLineChartView
+
+class LineChartJSONView(BaseLineChartView):
+    template_name='posts/analytics.html'
+    def get_labels(self):
+        """Return 7 labels for the x-axis."""
+        return ["January", "February", "March", "April", "May", "June", "July"]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Central", "Eastside", "Westside"]
+
+    def get_data(self):
+        """Return 3 datasets to plot."""
+
+        return [[75, 44, 92, 11, 44, 95, 35],
+                [41, 92, 18, 3, 73, 87, 92],
+                [87, 21, 94, 3, 90, 13, 65]]
